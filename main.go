b598ce349bc3e305ea1f980eb713820aad48263a
Bangalore Shankar Denanath, Chandan <49138440+chandanbsd@users.noreply.github.com>
2025-06-12 22:32:17 -0400

    Added exit and help commands

--- main.go
+++ main.go
@@ -7,6 +7,29 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+func init() {
+	commands = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
 func main() {
 
 	var word string
@@ -24,12 +47,36 @@ func main() {
 			cleanedSlice = cleanInput(word)
 
 			if len(cleanedSlice) > 0 {
-				fmt.Printf("Your command was: %s\n", cleanedSlice[0])
+				commandStr := cleanedSlice[0]
+
+				command, ok := commands[commandStr]
+
+				if !ok {
+					fmt.Println("Unknown command")
+				} else {
+					command.callback()
+				}
 			}
 
 		}
 
 	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+
+	fmt.Println("Welcome to the Pokedex!\nUsage: \n")
+
+	for _, command := range commands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
 }
 
 func cleanInput(text string) []string {
